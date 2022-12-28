2daf3373198b3a125afbc42dfa2b70e983edcb94
Rohan Arni <50297222+rtenacity@users.noreply.github.com>
2022-12-27 19:31:25 -0500

    finished module 0.0.1 official release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.8'
+VERSION = '0.0.1'
 DESCRIPTION = 'Access the QBreader API through Python.'
 
 # Setting up
@@ -19,10 +19,11 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
+    url = "https://github.com/qbreader/python-module",
     install_requires=['requests'],
     #keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
