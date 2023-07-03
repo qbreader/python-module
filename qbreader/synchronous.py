d0705dfbfae566e5d2d396fc2ef43972e0df4998
Sky "g3ner1c" Hong <g3ner1c-sky@pm.me>
2023-07-02 23:47:13 -0400

    finished migrating `query`

--- qbreader/synchronous.py
+++ qbreader/synchronous.py
@@ -1,71 +1,140 @@
 """Synchronous API functions."""
 
+from typing import Optional, Type, Union
+
 import requests
 
-BASE_URL = "https://www.qbreader.org/api"
+import qbreader.api_utils as api_utils
+from qbreader.api_utils import BASE_URL
+from qbreader.types import (
+    Bonus,
+    Category,
+    Difficulty,
+    QuestionType,
+    SearchType,
+    Subcategory,
+    Tossup,
+    UnnormalizedCategory,
+    UnnormalizedDifficulty,
+    UnnormalizedSubcategory,
+)
 
 
 def query(
-    questionType: str = "all",
-    searchType: str = "all",
-    queryString: str = "",
-    regex: bool = False,
-    randomize: bool = False,
-    setName: str = "",
-    difficulties: list = [],
-    categories: list = [],
-    subcategories: list = [],
-    maxQueryReturnLength: int = None,
+    questionType: QuestionType = "all",
+    searchType: SearchType = "all",
+    queryString: Optional[str] = "",
+    exactPhrase: Optional[bool] = False,
+    ignoreDiacritics: Optional[bool] = False,
+    regex: Optional[bool] = False,
+    randomize: Optional[bool] = False,
+    setName: Optional[str] = None,
+    difficulties: UnnormalizedDifficulty = None,
+    categories: UnnormalizedCategory = None,
+    subcategories: UnnormalizedSubcategory = None,
+    maxReturnLength: Optional[int] = 25,
+    tossupPagination: Optional[int] = 1,
+    bonusPagination: Optional[int] = 1,
 ) -> dict:
     """
-    Search the QBreader database.
+    Query the qbreader database for questions.
 
-    This function searches the QBreader database for questions that match the parameters specified.
+    Original API doc at https://www.qbreader.org/api-docs/query.
 
     Parameters
     ----------
-    questionType : str, must be one of "all", "tossup", "bonus"
-        The type of question to search for. Defaults to "all". If one of the three is not set, returns a 400 Bad Request.
-    searchType : str, must be one of "all", "answer", "question"
-        The type of search to perform. Defaults to "all". If one of the three is not set, returns a 400 Bad Request.
-    queryString : str (optional)
-        The string to search for. Defaults to "".
-    regex : str (optional)
-        Whether or not to use regular expressions for the queryString. Defaults to "false".
-    randomize : str (optional)
-        Whether or not to randomize the order of the results. Defaults to "false".
-    setName : str (optional)
-        The name of the set to search. Defaults to "". Leave as an empty string to search all.
-    difficulties : list (optional)
-        The difficulties to search for. Defaults to []. Leave as an empty list to search all. Must be a list of ints from 1 to 10.
-    categories : list (optional)
-        The categories to search for. Defaults to []. Leave as an empty list to search all.
-    subcategories : list (optional)
-        The subcategories to search for. Defaults to []. Leave as an empty list to search all.
-    maxQueryReturnLength : int (optional)
-        The maximum number of questions to return. Defaults to None. Leave blank to return 50. Anything over 200 will not work.
+    questionType : qbreader.types.QuestionType
+        The type of question to search for. Can be either a string or a question class
+        type.
+    searchType : qbreader.types.SearchType
+        Where to search for the query string. Can only be a string.
+    queryString : str, optional
+        The string to search for.
+    exactPhrase : bool, default = False
+        Ensure that the query string is an exact phrase.
+    ignoreDiacritics : bool, default = False
+        Ignore or transliterate diacritical marks in `queryString`.
+    regex : bool, default = False
+        Treat `queryString` as a regular expression.
+    randomize : bool, default = False
+        Randomize the order of the returned questions.
+    setName : str, optional
+        The name of the set to search in.
+    difficulties : qbreader.types.UnnormalizedDifficulty, optional
+        The difficulties to search for. Can be a single or an array of `Difficulty`
+        enums, strings, or integers.
+    categories : qbreader.types.UnnormalizedCategory, optional
+        The categories to search for. Can be a single or an array of `Category` enums or
+        strings.
+    subcategories : qbreader.types.UnnormalizedSubcategory, optional
+        The subcategories to search for. Can be a single or an array of `Subcategory`
+        enums or strings. The API does not check for consistency between categories and
+        subcategories.
+    maxReturnLength : int, default = 25
+        The maximum number of questions to return.
+    tossupPagination : int, default = 1
+        The page of tossups to return.
+    bonusPagination : int, default = 1
+        The page of bonuses to return.
 
-    Returns
-    ----------
-    dict
-        A dictionary containing the results of the search.
 
     """
-    regex_converted = str(regex).lower()
-    random_converted = str(randomize).lower()
+    # normalize parameters
+    if questionType == Tossup:
+        questionType = "tossup"
+    elif questionType == Bonus:
+        questionType = "bonus"
+    if questionType not in ["tossup", "bonus", "all"]:
+        raise ValueError("questionType must be either 'tossup', 'bonus', or 'all'.")
+
+    if searchType not in ["question", "answer", "all"]:
+        raise ValueError("searchType must be either 'question', 'answer', or 'all'.")
+
+    if not isinstance(queryString, str):
+        raise TypeError(
+            f"queryString must be a string, not {type(queryString).__name__}."
+        )
+
+    for name, param in tuple(
+        zip(
+            ("exactPhrase", "ignoreDiacritics", "regex", "randomize"),
+            (exactPhrase, ignoreDiacritics, regex, randomize),
+        )
+    ):
+        if not isinstance(param, bool):
+            raise TypeError(f"{name} must be a boolean, not {type(param).__name__}.")
+
+    if setName is not None and not isinstance(setName, str):
+        raise TypeError(f"setName must be a string, not {type(setName).__name__}.")
+
+    for name, param in tuple(
+        zip(
+            ("maxReturnLength", "tossupPagination", "bonusPagination"),
+            (maxReturnLength, tossupPagination, bonusPagination),
+        )
+    ):
+        if not isinstance(param, int):
+            raise TypeError(f"{name} must be an integer, not {type(param).__name__}.")
+        elif param < 1:
+            raise ValueError(f"{name} must be at least 1.")
+
     url = BASE_URL + "/query"
 
     data = {
         "questionType": questionType,
         "searchType": searchType,
         "queryString": queryString,
-        "regex": regex_converted,
-        "randomize": random_converted,
+        "exactPhrase": api_utils.normalize_bool(exactPhrase),
+        "ignoreDiacritics": api_utils.normalize_bool(ignoreDiacritics),
+        "regex": api_utils.normalize_bool(regex),
+        "randomize": api_utils.normalize_bool(randomize),
         "setName": setName,
-        "categories": categories,
-        "subcategories": subcategories,
-        "difficulties": difficulties,
-        "maxQueryReturnLength": maxQueryReturnLength,
+        "difficulties": api_utils.normalize_diff(difficulties),
+        "categories": api_utils.normalize_cat(categories),
+        "subcategories": api_utils.normalize_subcat(subcategories),
+        "maxReturnLength": maxReturnLength,
+        "tossupPagination": tossupPagination,
+        "bonusPagination": bonusPagination,
     }
 
     response = requests.get(url, params=data)
