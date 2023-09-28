30cca131aab3302b0c4668ef7bcaec659e041849
Sky "g3ner1c" Hong <g3ner1c-sky@pm.me>
2023-09-27 23:25:17 -0400

    noqa solves everything

--- qbreader/__init__.py
+++ qbreader/__init__.py
@@ -1,13 +1,13 @@
 """The official qbreader API python wrapper.
 
-    .. note::
-        Even though useful type aliases defined in `qbreader.types` are reexported here,
-        they are not documented here by `sphinx-autodoc`, but are documented in the
-        `types` module.
+.. note::
+    Even though useful type aliases defined in `qbreader.types` are reexported here,
+    they are not documented here by `sphinx-autodoc`, but are documented in the
+    `types` module.
 
-        See also:
-            * https://github.com/sphinx-doc/sphinx/issues/8547
-            * https://github.com/sphinx-doc/sphinx/issues/1063
+    See:
+        * https://github.com/sphinx-doc/sphinx/issues/8547
+        * https://github.com/sphinx-doc/sphinx/issues/1063
 """
 
 import importlib.metadata
@@ -15,8 +15,7 @@ import importlib.metadata
 import qbreader.types as types
 from qbreader.asynchronous import Async
 from qbreader.synchronous import Sync
-from qbreader.types import *
-
+from qbreader.types import *  # noqa: F401, F403
 __version__ = importlib.metadata.version("qbreader")
 __all__ = (
     "Async",
@@ -25,6 +24,6 @@ __all__ = (
 )
 
 # add all symbols from qbreader.types to __all__
-__all__ += types.__all__
+__all__ += types.__all__  # type: ignore
 
 del importlib
